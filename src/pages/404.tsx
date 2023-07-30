355dba892ff1efe93a87099e9132450a40972807
‘muddyrain’ <qiu3291002845@gmail.com>
2023-07-30 21:02:17 +0800

    feat(404): 添加404页面

---
+++ src/pages/404.tsx
@@ -0,0 +1,40 @@
+import { Box, Button, Container, Grid, Stack, Typography } from '@mui/material';
+import Image from 'next/image';
+import { FC } from 'react';
+
+export default function NotFound() {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '100vh',
+      }}
+    >
+      <Container maxWidth='md'>
+        <Stack direction='row' spacing={6}>
+          <Stack spacing={2}>
+            <Typography variant='h1'>404</Typography>
+            <Typography variant='h6'>您要查找的页面不存在。</Typography>
+            <Button
+              className='mt-4'
+              variant='contained'
+              onClick={() => {
+                history.go(-1);
+              }}
+            >
+              返回
+            </Button>
+          </Stack>
+          <Image
+            src='https://muddyrain-oss.oss-cn-hangzhou.aliyuncs.com/website/not-found-404.jpg'
+            width={500}
+            height={250}
+            alt='404'
+          />
+        </Stack>
+      </Container>
+    </Box>
+  );
+}
