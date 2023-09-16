503a42ab2c5cd837242f9c040be5ffd232b9e49d
muddyrain <qiu3291002845@gmail.com>
2023-09-16 20:21:32 +0800

    feat(music): 添加 音乐详情唱片走马灯效果

--- src/pages/music/components/SongDetail/index.tsx
+++ src/pages/music/components/SongDetail/index.tsx
@@ -1,15 +1,23 @@
 import { useMusicStore } from '@/store/useMusicStore'
-import { FC } from 'react'
+import { FC, useRef, useState } from 'react'
 import { Background } from './background'
 import styles from './index.module.scss'
 import { KeyboardArrowDown, KeyboardArrowUp, Pause, PlayArrow } from '@mui/icons-material'
 import { IconButton } from '@mui/material'
 import Image from 'next/image'
 import { testImg } from '@/assets'
+import { Swiper, SwiperRef, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+
 export const SongDetail: FC = () => {
+  const [currentIndex, setCurrentIndex] = useState(1)
   const { isShowSongDetail, playState, setPlayState } = useMusicStore(state => ({
     ...state,
   }))
+  const swiperRef = useRef<SwiperRef>(null)
+  const handleClick = (index: number) => {
+    swiperRef.current?.swiper.slideTo(index)
+  }
   return (
     <div
       className={`absolute overflow-hidden z-10 w-full h-full bg-red-200 duration-300 ${
@@ -22,41 +30,80 @@ export const SongDetail: FC = () => {
       <div className="w-full h-full absolute top-0 left-0 z-10">
         <div className={`${styles.rect_container} flex`}>
           <div className="flex flex-col w-1/2 justify-center items-center">
-            <IconButton>
-              <KeyboardArrowUp className="text-2xl" />
+            <IconButton
+              onClick={() => {
+                if (currentIndex > 0) {
+                  swiperRef.current?.swiper.slideTo(currentIndex - 1)
+                }
+              }}
+            >
+              <KeyboardArrowUp className="text-[64px]" />
             </IconButton>
             <IconButton color="primary">
-              <div className="w-16 h-16 bg-white/30 bg-opacity-50 p-[10px] rounded-full cursor-pointer">
+              <div className="w-24 h-24 bg-white/30 bg-opacity-50 p-[10px] rounded-full cursor-pointer">
                 <div className="w-full h-full rounded-full bg-white flex justify-center items-center">
-                  {playState === 'playing' ? <Pause /> : <PlayArrow />}
+                  {playState === 'playing' ? (
+                    <Pause className="text-4xl" />
+                  ) : (
+                    <PlayArrow className="text-4xl" />
+                  )}
                 </div>
               </div>
             </IconButton>
-            <IconButton>
-              <KeyboardArrowDown className="text-2xl" />
+            <IconButton
+              onClick={() => {
+                if (currentIndex < 10) {
+                  swiperRef.current?.swiper.slideTo(currentIndex + 1)
+                }
+              }}
+            >
+              <KeyboardArrowDown className="text-[64px]" />
             </IconButton>
           </div>
         </div>
-        <div className="w-1/4 absolute flex flex-col items-center justify-center h-full left-1/4">
-          <div
-            className={`w-52 h-52 absolute top-[0] translate-y-[-25%] rounded-full p-4 cursor-pointer`}
+        <div className="w-1/4 group absolute flex flex-col items-center justify-center h-full left-1/4">
+          <Swiper
+            ref={swiperRef}
+            className="w-full mx-auto"
+            slidesPerView={3}
+            initialSlide={currentIndex}
+            centeredSlides={true}
+            direction={'vertical'}
+            onSlideChange={e => {
+              setCurrentIndex(e.activeIndex)
+            }}
           >
-            <Image src={testImg} className="w-full h-full rounded-full opacity-75" alt="ball_img" />
-          </div>
-          <div
-            className={`w-64 h-64 rounded-full p-4 cursor-pointer bg-gradient-to-br from-pink-400 to-purple-300 ${styles.ball_container}`}
-          >
-            <Image
-              src={testImg}
-              className="w-full h-full rounded-full opacity-75 hover:animate-spin"
-              alt="ball_img"
-            />
-          </div>
-          <div
-            className={`w-52 h-52 absolute top-[100%] translate-y-[-75%] rounded-full p-4 cursor-pointer`}
-          >
-            <Image src={testImg} className="w-full h-full rounded-full opacity-75" alt="ball_img" />
-          </div>
+            {Array.from({ length: 10 }).map((item, index) => {
+              return (
+                <SwiperSlide
+                  className="h-1/3 flex justify-center items-center"
+                  key={index}
+                  virtualIndex={index}
+                >
+                  {({ isActive, isNext, isPrev }) => (
+                    <div className="w-full h-full flex justify-center items-center">
+                      <div
+                        onClick={() => handleClick(index)}
+                        className={`mx-auto rounded-full  cursor-pointer duration-300 ${
+                          isActive ? styles.ball_container : null
+                        } ${
+                          isActive
+                            ? 'w-64 h-64 opacity-100 bg-gradient-to-br p-4 from-pink-400 to-purple-300'
+                            : 'w-40 h-40 opacity-50 bg-gradient-to-br p-1 from-pink-400 to-purple-300'
+                        } ${isNext ? 'translate-y-8' : ''} ${isPrev ? '-translate-y-8' : ''}`}
+                      >
+                        <Image
+                          src={testImg}
+                          className="w-full h-full rounded-full opacity-75"
+                          alt="ball_img"
+                        />
+                      </div>
+                    </div>
+                  )}
+                </SwiperSlide>
+              )
+            })}
+          </Swiper>
         </div>
       </div>
     </div>
