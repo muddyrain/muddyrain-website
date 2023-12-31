e284cf7d563645fbec2488fe66779d0aa5017f6d
muddyrain <qiu3291002845@gmail.com>
2023-12-31 20:51:01 +0800

    fix(favicon): 修复图标丢失问题

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Layout } from '@/Layout'
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
       <body>
         <Layout>{children}</Layout>
       </body>
